5031adee2198d636e2432217359667c40ee25026
Kevin Amado <kamadorueda@gmail.com>
2021-08-29 19:25:11 -0500

    feat(build): public api

--- README.md
+++ README.md
@@ -19,7 +19,47 @@ Packaging every package on PyPI.
 
 ## Using
 
-:construction: work in progress
+### Importing
+
+In order to use this repository you need to import it
+into your project. For example:
+
+```nix
+let
+  nixpkgs = import <nixpkgs> { };
+
+  nixpkgsPython =
+    let src = nixpkgs.fetchFromGitHub {
+      owner = "kamadorueda";
+      repo = "nixpkgs-python";
+      # Pick one from this list:
+      #   https://github.com/kamadorueda/nixpkgs-python/commits/main
+      rev = "ee68d41b3ca8e180a65bcb78c7375dc962162e3a";
+      # Update this manually
+      sha256 = "0000000000000000000000000000000000000000000000000000";
+    };
+    in import src { };
+in
+# Keep reading for more information
+```
+
+### makeEnv
+
+Cook an environment where the desired [Python][PYTHON] packages
+are present.
+
+Example:
+
+```nix
+nixpkgsPython.makeEnv {
+  pkgs = [
+    "attrs"
+  ];
+  pythonVersion = "3.7";
+}
+```
+
+Valid `pythonVersion`s are: `3.6`, `3.7` and `3.8`.
 
 ---
 
--- default.nix
+++ default.nix
@@ -1,26 +1,19 @@
 { nixpkgs ? import <nixpkgs> { }
 }:
 let
-  # Import: https://github.com/fluidattacks/makes
-  makesSrc = nixpkgs.fetchzip {
-    url = "https://github.com/fluidattacks/makes/archive/5c973a6031a5858200d1524d8248401d5922a38b.tar.gz";
-    sha256 = "02kf2qikvlqib5vqrcz0zdwkm90wpw9j60m02170gvy2n115g0x8";
-  };
-  makes = import "${makesSrc}/src/args/agnostic.nix" { };
-
   pkgsSrc = ./pkgs;
 
-  ls = dir: builtins.attrNames (builtins.readDir dir);
 
   buildProjects = pythonVersion:
     builtins.foldl'
-      (projects: project: projects // (buildProject project))
+      (projects: project: projects // (buildProject pythonVersion project))
       { }
       (ls pkgsSrc);
 
-  buildProject = project:
+
+  buildProject = pythonVersion: project:
     let
-      versions = buildProjectVersions project;
+      versions = buildProjectVersions pythonVersion project;
       latest = builtins.head
         (builtins.sort
           (a: b: (builtins.compareVersions a b) > 0)
@@ -33,15 +26,80 @@ let
     # Latest version
     { "${project}" = versions.${latest}; };
 
-  buildProjectVersions = project:
+
+  buildProjectVersions = pythonVersion: project:
     builtins.foldl'
-      (versions: version: versions // { "${version}" = null; })
+      (versions: version:
+        versions // (buildProjectVersion pythonVersion project version))
       { }
       (ls (pkgsSrc + "/${project}"));
+
+
+  buildProjectVersion = pythonVersion: project: version:
+    let
+      name = "python${pythonVersion}-${project}-${version}";
+      closure = pkgsSrc + "/${project}/${version}/closure-${pythonVersion}.json";
+      links = pkgsSrc + "/${project}/${version}/files.json";
+
+      venv = makes.makePythonPypiEnvironment {
+        inherit name;
+        sourcesYaml = makes.toFileYaml "${name}-sources.yaml" {
+          closure = makes.fromJsonFile closure;
+          links = makes.fromJsonFile links;
+          python = pythonVersion;
+        };
+      };
+    in
+    { "${version}" = venv; };
+
+
+  makeEnv =
+    { pkgs
+    , pythonVersion
+    ,
+    }:
+    makes.makeSearchPaths {
+      source = builtins.map
+        (project: builtProjects.${pythonVersion}.${project})
+        (pkgs);
+    };
+
+  builtProjects = {
+    "3.6" = buildProjects "3.6";
+    "3.7" = buildProjects "3.7";
+    "3.8" = buildProjects "3.8";
+    "3.9" = buildProjects "3.9";
+  };
+
+  __all__ =
+    let
+      makeEnvFor = pythonVersion:
+        makeEnv {
+          pkgs = builtins.attrNames builtProjects.${pythonVersion};
+          inherit pythonVersion;
+        };
+    in
+    nixpkgs.linkFarm "nixpkgs-python"
+      (builtins.map
+        (pythonVersion: {
+          name = pythonVersion;
+          path = makeEnvFor pythonVersion;
+        })
+        [ "3.6" "3.7" "3.8" "3.9" ]);
+
+  ls = dir: builtins.attrNames (builtins.readDir dir);
+  makes =
+    let src = nixpkgs.fetchzip {
+      url = "https://github.com/fluidattacks/makes/archive/5c973a6031a5858200d1524d8248401d5922a38b.tar.gz";
+      sha256 = "02kf2qikvlqib5vqrcz0zdwkm90wpw9j60m02170gvy2n115g0x8";
+    };
+    in import "${src}/src/args/agnostic.nix" { };
 in
 {
-  python36Packages = buildProjects "3.6";
-  python37Packages = buildProjects "3.7";
-  python38Packages = buildProjects "3.8";
-  python39Packages = buildProjects "3.9";
+  inherit __all__;
+  inherit makeEnv;
+  python36Packages = builtProjects."3.6";
+  python37Packages = builtProjects."3.7";
+  python38Packages = builtProjects."3.8";
+  python39Packages = builtProjects."3.9";
 }
