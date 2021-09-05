a09b458e9244d8dc5a7de29f79ceef88522422a8
Kevin Amado <kamadorueda@gmail.com>
2021-09-05 17:42:23 -0500

    feat(build): generate ci

--- .github/workflows/generate.py
+++ .github/workflows/generate.py
@@ -10,46 +10,42 @@ def main() -> None:
         },
         "name": "ci",
         "on": ["pull_request", "push"],
-        "jobs": {
-            "build": {
-                "env": {
-                    "CACHIX_AUTH_TOKEN": "${{ secrets.CACHIX_AUTH_TOKEN }}"
-                },
-                "runs-on": "ubuntu-latest",
-                "steps": [
-                    {"uses": "actions/checkout@v2"},
-                    {"uses": "cachix/install-nix-action@v13"},
-                    {
-                        "run": "nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs"
-                    },
-                    {"run": "nix-channel --update"},
-                    {"run": "nix-env -i cachix"},
-                    {"run": "nix-env -i just"},
-                ],
-                "strategy": {
-                    "matrix": {
-                        "python": [
-                            "python36",
-                            "python37",
-                            "python38",
-                            "python39",
-                        ]
-                    }
-                },
-            }
-        },
+        "jobs": {},
     }
 
-    for project in sorted(os.listdir("projects")):
-        project_path = os.path.join("projects", project)
-        for version in sorted(os.listdir(project_path)):
-            version_path = os.path.join(project_path, version)
-            if os.path.isdir(version_path):
-                data["jobs"]["build"]["steps"].append(
-                    {
-                        "run": f"just build '${{{{ matrix.python }}}}.\"{project}-{version}\"'",
-                    }
-                )
+    closures = {
+        "python36": "python3.6.json",
+        "python37": "python3.7.json",
+        "python38": "python3.8.json",
+        "python39": "python3.9.json",
+    }
+    for python in closures:
+        data["jobs"][python] = {
+            "env": {"CACHIX_AUTH_TOKEN": "${{ secrets.CACHIX_AUTH_TOKEN }}"},
+            "runs-on": "ubuntu-latest",
+            "steps": [
+                {"uses": "actions/checkout@v2"},
+                {"uses": "cachix/install-nix-action@v13"},
+                {
+                    "run": "nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs"
+                },
+                {"run": "nix-channel --update"},
+                {"run": "nix-env -i cachix"},
+                {"run": "nix-env -i just"},
+            ],
+        }
+        for project in sorted(os.listdir("projects")):
+            project_path = os.path.join("projects", project)
+            for version in sorted(os.listdir(project_path)):
+                version_path = os.path.join(project_path, version)
+                if os.path.isdir(version_path) and os.path.exists(
+                    os.path.join(version_path, closures[python])
+                ):
+                    data["jobs"][python]["steps"].append(
+                        {
+                            "run": f"just build '{python}.\"{project}-{version}\"'",
+                        }
+                    )
 
     with open(".github/workflows/ci.yml", "w") as file:
         yaml.dump(data, file, indent=2, sort_keys=True)
