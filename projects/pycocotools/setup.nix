303774fd31d33a484f63f3c13d5f1d4977a70e5f
Kevin Amado <kamadorueda@gmail.com>
2021-11-02 17:24:55 -0500

    feat(conf): pycocotools

--- projects/pycocotools/setup.nix
+++ projects/pycocotools/setup.nix
@@ -1,6 +1,7 @@
 {
   buildGccBin = true;
-  searchPathsBuild = { nixpkgs, pythonOnNix, pythonVersion }: {
+  buildSetuptools_57_5_0 = true;
+  searchPathsBuild = { nixpkgs, pythonOnNix, pythonVersion, ... }: {
     source = [
       pythonOnNix.projects.cython.latest.${pythonVersion}.dev
       pythonOnNix.projects.numpy.latest.${pythonVersion}.dev
