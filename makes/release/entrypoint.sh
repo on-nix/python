d8a7fa21b76ac3b8a1a3fedb41e86352769b09ed
Kevin Amado <kamadorueda@gmail.com>
2022-04-19 12:20:11 -0600

    feat(conf): music21

--- makes/release/entrypoint.sh
+++ makes/release/entrypoint.sh
@@ -4,17 +4,20 @@ function main {
   local project="${1}"
   local pwd="${PWD}"
 
-  echo -n > "projects/${project}/test.py" \
-    && just build "projects.${project}.latest.pythonLatest" \
-    && base="$(echo "result-3/lib/python"*"/site-packages")" \
-    && pushd "${base}" \
-    && find . -maxdepth 2 -wholename "*/${project}*/__init__.py" \
-    | while read -r path; do
-      path="$(dirname "${path}")" \
-        && path="${path//\//.}" \
-        && echo "import "${path:2}"" >> "${pwd}/projects/${project}/test.py"
-    done \
-    && popd \
+  : \
+    && if ! test -e "projects/${project}/test.py"; then
+      echo -n > "projects/${project}/test.py" \
+        && just build "projects.${project}.latest.pythonLatest" \
+        && base="$(echo "result-3/lib/python"*"/site-packages")" \
+        && pushd "${base}" \
+        && find . -maxdepth 2 -name __init__.py \
+        | while read -r path; do
+          path="$(dirname "${path}")" \
+            && path="${path//\//.}" \
+            && echo "import "${path:2}"" >> "${pwd}/projects/${project}/test.py"
+        done \
+        && popd
+    fi \
     && just build "projects.${project}" \
     && git add "projects/${project}" \
     && git commit -m "feat(conf): ${project}"
